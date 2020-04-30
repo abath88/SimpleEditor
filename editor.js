7dd2082f35a31f445cc312b715d6b2a599ae2db7
Albert Czarnecki <abath@vp.pl>
2020-04-30 16:21:51 +0200

    War, war never change!

--- editor.js
+++ editor.js
@@ -104,16 +104,23 @@ function Editor(settings) {
 
 
     let bold = document.getElementById("bold")
+
+
     bold.addEventListener('click', (e) => {
-        let range = window.getSelection().getRangeAt(0)
-        let startEl = getElementFromRange(range.startContainer)
-        let endEl = getElementFromRange(range.endContainer)
+        let selection = getSelection()
+
+        let startEl = selection.startNode
+        let endEl = selection.endNode
+
+        //nazewnictwo z selection
 
         let rangeClone = {
-            startOffset: range.startOffset,
-            endOffset: range.endOffset,
-            startContainer: startEl,
-            endContainer: endEl,
+            startOffset: selection.startOffset,
+            endOffset: selection.endOffset,
+            startNode: startEl,
+            endNode: endEl,
+            startBlock: startEl.parentElement,
+            endBlock: endEl.parentElement,
         };
 
         if(startEl === endEl){
@@ -152,31 +159,133 @@ function Editor(settings) {
         newRange.setStart(rangeClone.startContainer, 0)
         newRange.setEnd(rangeClone.endContainer, rangeClone.endOffset);
         
-        let selection = window.getSelection()
-        selection.removeAllRanges()
-        selection.addRange(newRange)
+        let nselection = window.getSelection()
+        nselection.removeAllRanges()
+        nselection.addRange(newRange)
 
     })
 
-    function getElementFromRange(node) {
-        let currentEl = isElement(node)
-          ? node
-          : node.parentElement;
-        currentEl = isModifier(currentEl) ? currentEl.parentElement : currentEl;
-        return currentEl
+
+    /* Utils Functions */
+    var getElementFromNode = (node) => 
+        isElement(node) ? node : node.parentElement
+
+    var isElement = (node) => 
+        node.nodeType === Node.ELEMENT_NODE ?  true : false
+    
+    var getSelection = () => {
+        let selection = editor.ownerDocument.getSelection().getRangeAt(0)
+
+        let startNode = getElementFromNode(selection.startContainer)
+        startNode = getTextElement(startNode)
+        let endNode = getElementFromNode(selection.endContainer)
+        endNode = getTextElement(endNode)
+        let startBlock = startNode.parentElement
+        let endBlock = endNode.parentElement;
+
+        return {
+            startNode,
+            endNode,
+            startBlock,
+            endBlock,
+            startOffset: selection.startOffset,
+            endOffset: selection.endOffset,
+            isCollapsed: editor.ownerDocument.getSelection().isCollapsed
+        }
     }
 
-    function isElement(node) {
-        if(node.nodeName === '#text') return false
-        else return true
+    var setSelection = (newSelection) => {
+        let selection = editor.ownerDocument.getSelection()
+        let range = document.createRange()
+
+        range.setStart(
+            newSelection.startNode.childNodes[0], 
+            newSelection.startOffset
+        )
+        range.setEnd(
+            newSelection.endNode.childNodes[0],
+            newSelection.endOffset
+        );
+
+        selection.removeAllRanges()
+        selection.addRange(range)
     }
 
-    function isModifier(node){
-        if(node.nodeName === 'SPAN') return true
-        else return false
+    var insertAfter = (newNode, node) => {
+      node.parentElement.insertBefore(newNode, node.nextSibling);
+    };
+
+    var getTextElement = (node) =>
+        isText(node) ? node : node.parentElement
+
+    var isText = (node) =>
+        (node.dataset.hasOwnProperty('type') && 
+            node.dataset.type === 'text')  
+                ? true : false
+
+
+    var addEmptyLine = (selection) => {
+        let block = elementWithChilds('p', [
+            {
+                type: 'span',
+                content: '<br>',
+                data: 'text',
+            }
+        ]);
+
+        insertAfter(block, selection.startBlock);
+
+        setSelection({
+            startNode: block.childNodes[0],
+            endNode: block.childNodes[0],
+            startOffset: 0,
+            endOffset: 0, 
+        })
     }
 
-    /* END SETUP TOOLBAR */ 
+
+    /* OD TEGO MIEJSCA ZACZAĆ */
+    /* !!!!! */
+    var deleteFromSelection = (selection) => {
+          let currBlock =
+            selection.startBlock == selection.endBlock
+              ? selection.startBlock
+              : selection.startBlock.nextSibling;
+
+          while (currBlock != selection.endBlock) {
+            let next = currBlock.nextSibling;
+            editor.removeChild(currBlock);
+            currBlock = next;
+          }
+
+          let currNode = selection.startNode.nextSibling;
+
+          if (selection.startBlock != selection.endBlock) {
+            while (currNode != null) {
+              let next = currNode.nextSibling;
+              selection.startBlock.removeChild(currNode);
+              currNode = next;
+            }
+          }
+
+          currNode = selection.endBlock.firstChild;
+          while (currNode != selection.endNode) {
+            let next = currNode.nextSibling;
+            selection.endBlock.removeChild(currNode);
+            currNode = next;
+          }
+
+          selection.startNode.textContent = selection.startNode.textContent.substring(
+            0,
+            selection.startOffset
+          );
+          selection.endNode.textContent = selection.endNode.textContent.substring(
+            selection.endOffset,
+            selection.endNode.textContent.length
+          );
+    }
+    /* End Utils */
+
     editor.addEventListener('click', (e) => {
         cursor.el = window.getSelection().getRangeAt(0).startContainer;
         cursor.offset = window.getSelection().getRangeAt(0).startOffset;
@@ -188,22 +297,23 @@ function Editor(settings) {
                 if(editor.children.length == 1 && editor.children[0].textContent.length == 0) {
                         e.preventDefault()
                 }
+            break;
 
             case 'Enter':
                 e.preventDefault()
-                let selection = window.getSelection()
-                let block = null
-                if(selection.isCollapsed){
-                    block = elementWithChilds('p', [
-                      {
-                        type: 'span',
-                        content: '<br>',
-                        data: 'decorator',
-                      }
-                    ]);
-                }
-                console.log(block)
-                editor.appendChild(block)
+                let selection = getSelection()
+
+                if(!selection.isCollapsed)
+                    deleteFromSelection(selection)
+                else 
+                    setSelection({
+                        startNode: selection.startNode,
+                        endNode: selection.startNode,
+                        startOffset: selection.startNode.textContent.length,
+                        endOffset: selection.startNode.textContent.length,
+                    });
+
+                addEmptyLine(getSelection())
             default:
                 break;
         }
@@ -243,6 +353,3 @@ var richTextEditor = new Editor({
   placeholder: 'Rozpocznij pisanie artykułu',
 });
 
-function isElement(node) {
-    return node.nodeType === Node.ELEMENT_NODE
-}
